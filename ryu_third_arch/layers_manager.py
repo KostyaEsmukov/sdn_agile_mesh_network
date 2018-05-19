e0895d080f2abadd5785a3302d807616927873b6
Kostya Esmukov <kostya@esmukov.ru>
2018-05-19 21:01:38 +0300

    WIP 3rd arch: layers manager structure

--- ryu_third_arch/layers_manager.py
+++ ryu_third_arch/layers_manager.py
@@ -27,13 +27,20 @@ class LocalControlChannel:
 
     async def start_server(self, loop):
         self.server = await asyncio.start_unix_server(
-            self._handle_client, self.socket_path, loop=loop)
+            self._handle_accept, self.socket_path, loop=loop)
 
     async def close_wait(self):
         self.server.close()
         await self.server.wait_closed()
 
-    async def _handle_client(self, reader, writer):
+    async def _handle_accept(self, reader, writer):
+        # TODO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        # dump state
+        # create tunnel
+        # (back) - new tunnel created
+
+        # keep open connections (notify from outside)
+        # mark command end
         print('hi')
         pass
 
@@ -49,18 +56,40 @@ class TunnelsState:
 
 
 class TcpBalancer:
-    # listen TCP sock (ovpn)
-    pass
+    def __init__(self, tcp_port, tcp_host='0.0.0.0'):
+        self.tcp_host = tcp_host
+        self.tcp_port = tcp_port
+        self.server = None
+
+    def __str__(self):
+        return f"TcpBalancer server at {self.tcp_host}:{self.tcp_port}"
+
+    async def start_server(self, loop):
+        self.server = await asyncio.start_server(self._handle_accept,
+                                                 self.tcp_host, self.tcp_port,
+                                                 loop=loop)
+
+    async def close_wait(self):
+        self.server.close()
+        await self.server.wait_closed()
+
+    async def _handle_accept(self, reader, writer):
+        print('hi')
+        pass
 
 
 
 def main():
     logger.info('Starting...')
     loop = asyncio.get_event_loop()
+
     local_control = LocalControlChannel()
-    coro = local_control.start_server(loop=loop)
     logger.info(f'Starting {local_control}')
-    loop.run_until_complete(coro)
+    loop.run_until_complete(local_control.start_server(loop=loop))
+
+    openvpn_balancer = TcpBalancer(1194)
+    logger.info(f'Starting {openvpn_balancer}')
+    loop.run_until_complete(openvpn_balancer.start_server(loop=loop))
 
     try:
         logger.info('Running forever...')
@@ -68,9 +97,12 @@ def main():
     except KeyboardInterrupt:
         pass
 
+    logger.info(f'Closing {openvpn_balancer}...')
+    loop.run_until_complete(openvpn_balancer.close_wait())
     logger.info(f'Closing {local_control}...')
     loop.run_until_complete(local_control.close_wait())
     loop.close()
+
 
 if __name__ == '__main__':
     main()
